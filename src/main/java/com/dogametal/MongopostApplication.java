81bc3937fe43a16ed9ad1ba37ae5ac55764ae89e
dogametal <dogametal@hotmail.com>
2021-03-25 06:17:24 -0300

    mongo created

---
+++ src/main/java/com/dogametal/MongopostApplication.java
@@ -0,0 +1,23 @@
+package com.dogametal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class MongopostApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "MongoDB Connection !";
+    }
+    
+	public static void main(String[] args) {
+		SpringApplication.run(MongopostApplication.class, args);
+	}
+
+}
